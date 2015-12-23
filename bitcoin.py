d04031251ac770497d97f2cd3e4765468f222dae
shindi <shindi@haverford.edu>
2015-12-23 01:36:33 -0500

    Wrote notes on how bitcoin works. Might make my own cryptocurrency depending on how bored I am

---
+++ bitcoin.py
@@ -0,0 +1,27 @@
+'''
+Alice sends money to Bob
+  Alice has private key
+  Uses private key to generate public key (SendTo Address) for every transaction
+  Pub Key + Message = Sig. Sig authenticates the transaction
+    Uses Elliptic Curve Digital Signature Algorithm
+    Uses Mathematical Trap Door
+  To send money, you must have your private key
+  To receive money you must have their public key
+All the miners mark this transaction in their ledger
+When you first download bitcoin wallet software you must verify all the transactions ever passed
+Nodes must check every transaction ever made for every input to make sure it is unspent
+  This is made easier with an index of unspent nodes
+
+Your Balance = All the inputs sent to you that have not been spent ie sent out
+
+Send Money = Take unspent transactions until they total near the money you want to send. Any left
+over is sent to you
+
+When you
+
+
+
+
+
+
+'''
